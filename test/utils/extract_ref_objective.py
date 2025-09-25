659aae2f29547579cb4a77a8b868d6da49b78ab2
ekatef <e.v.kasilova@gmail.com>
2025-09-25 08:38:12 +0200

    Remove testing prints

--- test/utils/extract_ref_objective.py
+++ test/utils/extract_ref_objective.py
@@ -52,21 +52,24 @@ def extract_obj_for_path(dir, fl, ref_df=REF_OBJ_DF):
 
     return obj_value
 
+# print("REF_OBJ_DF")
+# print(REF_OBJ_DF)    
 
-print("test 1")
-print(
-   extract_obj_for_path(
-       "custom",
-       "elec_s_6_ec_lcopt_Co2L-4H_python.log",
-       ref_df=REF_OBJ_DF
-   )
-)
 
-print("test 2")
-print(
-   extract_obj_for_path(
-       "custom2",
-       "elec_s_6_ec_lcopt_Co2L-4H_python.log",
-       ref_df=REF_OBJ_DF
-   )
-)
+# print("test 1")
+# print(
+#    extract_obj_for_path(
+#        "custom",
+#        "elec_s_6_ec_lcopt_Co2L-4H_python.log",
+#        ref_df=REF_OBJ_DF
+#    )
+# )
+
+# print("test 2")
+# print(
+#    extract_obj_for_path(
+#        "custom2",
+#        "elec_s_6_ec_lcopt_Co2L-4H_python.log",
+#        ref_df=REF_OBJ_DF
+#    )
+# )
